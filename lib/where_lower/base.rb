05dd94d75204e234dbd6c9246898dba1124d080c
PikachuEXE <pikachuexe@gmail.com>
2014-03-20 15:44:21 +0800

    ~ Improve comment in code [ci skip]

--- lib/where_lower/base.rb
+++ lib/where_lower/base.rb
@@ -1,24 +1,44 @@
 require "where_lower/scope_spawner"
 
 module WhereLower
+  # Table name and column name seperator
+  # This should never change
   SEPERATOR = ".".freeze
 
+  # Raised when some deeply nested hash passed in which cannot be handled by this gem
   class TooDeepNestedConditions < ArgumentError; end
 
+  # Internal API to be called from extension methods
   module Base
     class << self
-      # @params [ActiveRecord::Relation, ActiveRecord::Base] scope
-      # @params [Hash] fields
+      # Spawn a new scope based on existing scope and hash
+      # This is method is for internal use
+      #
+      # @api private
+      #
+      # @params scope [ActiveRecord::Relation, ActiveRecord::Base]
+      #   the existing scope, ActiveRecord::Base also works since `where` will be delegated
+      # @params fields [Hash]
+      #   @see `.where_lower`
+      #
+      # @see `.spawn_lower_scope_by_type`
+      #
+      # @return [ActiveRecord::Relation]
       def spawn_lower_scope(scope, fields)
         fields.inject(scope) do |new_scope, (name, value)|
           spawn_lower_scope_by_type(new_scope, name, value)
         end
       end
 
-      # @params [ActiveRecord::Relation, ActiveRecord::Base] scope
-      # @params [String, Symbol] column_or_table_name When used with nested hash, this is table name
-      # @params [Object] value
-      # @params [String, Symbol] prefix used when doing recursive call
+      # This is method is for internal use
+      #
+      # @api private
+      #
+      # @params scope [ActiveRecord::Relation, ActiveRecord::Base]
+      #   @see spawn_lower_scope
+      # @params column_or_table_name [String, Symbol]
+      #   When used with nested hash, this is table name
+      # @params value [Object]
       #
       # @raise [TooDeepNestedConditions] if the conditions hash has is too deep nested
       #
--- lib/where_lower/version.rb
+++ lib/where_lower/version.rb
@@ -1,6 +1,10 @@
 module WhereLower
+  # String form of version, to be parsed by Gem::Version
   VERSION = "0.3.0"
 
+  # Return a version object instead of just a string for easier comparison
+  #
+  # @return [Gem::Version]
   def self.version
     Gem::Version.new(VERSION)
   end
